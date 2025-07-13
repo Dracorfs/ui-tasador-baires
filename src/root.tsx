38c90b04c1a4f586afd9346fb37ee60548d0183c
Dracorfs <franciscosantopietro@hotmail.com>
2025-07-13 20:44:09 -0300

    add tailwind

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,5 @@
 import { component$, isDev } from "@builder.io/qwik";
-import { QwikCityProvider, RouterOutlet } from "@builder.io/qwik-city";
+import { QwikCityProvider } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
@@ -25,7 +25,13 @@ export default component$(() => {
         <RouterHead />
       </head>
       <body lang="en">
-        <RouterOutlet />
+        <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+          <h1 class="text-4xl font-bold text-blue-600 mb-4">¡Tailwind test ok!</h1>
+          <p class="text-gray-700 text-lg">Usando Tailwind con Qwik 🎉</p>
+          <button class="mt-6 px-4 py-2 bg-green-500 hover:bg-green-600 text-white rounded">
+            Probar botón
+          </button>
+        </div>
       </body>
     </QwikCityProvider>
   );
